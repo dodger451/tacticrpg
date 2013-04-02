404b5749b665607194e68f0fc76ff8b9d697eefc
David Latotzky <david-git@latotzky.de>
2013-04-02 18:18:36 +0200

    introducing queue, fixing damage, better test-suit

--- js/libWowCombat.js
+++ js/libWowCombat.js
@@ -13,12 +13,12 @@ function Combatrules() {
 }
 
 /**
- * 
+ *
  * Pre-3.0. With Patch 3.0, both hit and spell hit use the same basic hit rating attribute within the combat rating system,
- * @see http://www.wowwiki.com/Miss 
+ * @see http://www.wowwiki.com/Miss
  */
 Combatrules.prototype.getMissChance = function(attacker, defender) {
-	
+
 	if (10 >= Math.abs(defender.getDefenseSkill() - attacker.getAttackSkill())) {
 		var basechance = 5;
 		if (attacker.isDualWielding()) {
@@ -31,38 +31,31 @@ Combatrules.prototype.getMissChance = function(attacker, defender) {
 			basechance = 25;
 		}
 		return basechance + (defender.getDefenseSkill() - attacker.getAttackSkill() - 10) * 0.4;
-	
 	}
-	
 }
-
 Combatrules.prototype.getDodgeChance = function(attacker, defender) {
 	return 5;
-	//TODO calc getDodgeChance
+	//TODO calc Combatrules.getDodgeChance
 }
-
 Combatrules.prototype.getParryChance = function(attacker, defender) {
 	return 5;
-	//TODO calc getParryChance
+	//TODO calc Combatrules.getParryChance
 }
-
 Combatrules.prototype.getGlancingBlowChance = function(attacker, defender) {
 	return 5;
-	//TODO calc getGlancingBlowChance
+	//TODO calc Combatrules.getGlancingBlowChance
 }
 Combatrules.prototype.getBlockChance = function(attacker, defender) {
 	return 5;
-	//TODO calc getBlockChance
+	//TODO calc Combatrules.getBlockChance
 }
-
 Combatrules.prototype.getCriticalHitChance = function(attacker, defender) {
 	return 5;
-	//TODO calc getCriticalHitChance
+	//TODO calc Combatrules.getCriticalHitChance
 }
-
 Combatrules.prototype.getCrushingBlowChance = function(attacker, defender) {
 	return 5;
-	//TODO calc getCrushingBlowChance
+	//TODO calc Combatrules.getCrushingBlowChance
 }
 
 Combatrules.prototype.getAutoAttackTable = function(attacker, defender) {
@@ -76,7 +69,7 @@ Combatrules.prototype.getAutoAttackTable = function(attacker, defender) {
 	attacktablebuilder.add(this.ATTACKRESULT_CRUSHING_BLOW, this.getCrushingBlowChance(attacker, defender));
 	attacktablebuilder.add(this.ATTACKRESULT_ORDINARY_HIT, 100);
 	var attacktable = attacktablebuilder.getTable();
-	
+
 	return attacktable;
 }
 Combatrules.prototype.getRangeAttackTable = function(attacker, defender) {
@@ -90,7 +83,7 @@ Combatrules.prototype.getRangeAttackTable = function(attacker, defender) {
 	attacktablebuilder.add(this.ATTACKRESULT_CRUSHING_BLOW, this.getCrushingBlowChance(attacker, defender));
 	attacktablebuilder.add(this.ATTACKRESULT_ORDINARY_HIT, 100);
 	var attacktable = attacktablebuilder.getTable();
-	
+
 	return attacktable;
 }
 
@@ -104,7 +97,7 @@ Combatrules.prototype.getAttackResult = function(attackTable) {
 		if (rnd < attackTable[i].maxrole) {
 			return attackTable[i].type;
 		}
-	}	
+	}
 	throw new Exception('unknown result for throw ' + rnd);
 }
 
@@ -154,15 +147,24 @@ AttackTableBuilder.prototype.add = function(pType, pChance) {
 AttackTableBuilder.prototype.getTable = function() {
 	return this._attackTable;
 }
-
 //
 /// Combatant
 //
-function Combatant() {
-	//TODO constructor for character (and rest)
+function Combatant(id) {
+	//TODO Combatant.constructor for character (rest attributes)
 	//create with dummy equipment
+	this._id = id;
 	this._level = 80;
-	this._currentAttackWeapon = new Weapon();
+	this._currentAttackWeapon = new Weapon({
+		isMeleeWeapon : true,
+		maxDamage : 15,
+		minDamage : 10,
+		speed : 2.0
+	});
+}
+
+Combatant.prototype.getId = function() {
+	return this._id;
 }
 
 Combatant.prototype.getLevel = function() {
@@ -174,98 +176,133 @@ Combatant.prototype.getLevel = function() {
 Combatant.prototype.isMob = function() {
 	return true;
 }
-
-
 Combatant.prototype.getAttackWeapon = function() {
 	return this._currentAttackWeapon;
 }
-
-
+Combatant.prototype.setAttackWeapon = function(weapon) {
+	this._currentAttackWeapon = weapon;
+}
 Combatant.prototype.getDefenseSkill = function() {
 	return 100;
-	//TODO calc getDefenseSkill
+	//TODO calc Combatant.getDefenseSkill
 }
 Combatant.prototype.getAttackSkill = function() {
 	return 100;
-	//TODO calc getAttackSkill
+	//TODO calc Combatant.getAttackSkill
 }
-
 Combatant.prototype.isDualWielding = function() {
 	return false;
-	//TODO calc isDualWielding
+	//TODO calc Combatant.isDualWielding
 }
 /**
  * Effective defensive skill for calculaions in combat
  */
 Combatant.prototype.getDefenseSkill = function() {
-	//TODO calc getDefenseSkill
-	// It is the Mob's level multiplied by 5. 
+	//TODO calc Combatant.getDefenseSkill
+	// It is the Mob's level multiplied by 5.
 	return 299;
 }
-
 /**
  * Effective melee attack skill for calculations in combat
  */
 Combatant.prototype.getAttackSkill = function() {
-	//TODO calc getAttackSkill depending on current attackWeapon inlcuding all mods
-	// It is the Mob's level multiplied by 5. 
+	//TODO calc Combatant.getAttackSkill depending on current attackWeapon inlcuding all mods
+	// It is the Mob's level multiplied by 5.
 	return 299;
 }
-
 /**
- * @see http://www.wowwiki.com/Damage_per_second 
+ * Effective melee attack power for calculating damage in combat
+ * http://www.wowwiki.com/Attack_power
+ */
+Combatant.prototype.getMeleeAttackPower = function() {
+	//TODO calc Combatant.getMeleeAttackPower based on attributes (Strength, agility,...) and all mods
+	return 28;
+	//dummy
+}
+/**
+ * Effective range attack power for calculating damage in combat
+ * http://www.wowwiki.com/Attack_power
+ */
+Combatant.prototype.getRangeAttackPower = function() {
+	//TODO calc Combatant.getRangeAttackPower based on attributes (Strength, agility,...) and all mods
+	return 28;
+	//dummy
+}
+/**
+ * @see http://www.wowwiki.com/Damage_per_second
  * Minimum Range = (((Minimum damage / Weapon Speed) + (Melee Attack Power / 14)) * (Weapon Speed)) * (Dual Wield Penalty)/
  * Minimum Range = (((Minimum damage + Scope damage bonus) / Weapon Speed) + (Ranged Attack Power / 14) + Ammo DPS bonus) * Weapon Speed
-
  */
-Combatant.prototype.getMinDps = function() {
+Combatant.prototype.getDamage = function(weaponDamage) {
 	var attackWeapon = this.getAttackWeapon();
-	var minDmg = 0;
+	var dmg = 0;
 	if (attackWeapon.isMelee()) {
-		//minDmg = ((attackWeapon.getMinDamage() / attackWeapon.getWeaponSpeed()) + (Melee Attack Power / 14))
-	} else { //range-weapon
-		
+		dmg = (
+			(weaponDamage / attackWeapon.getSpeed())  
+			+ (this.getMeleeAttackPower() / 14)
+			) * attackWeapon.getSpeed();
+	} else {//range-weapon
+		dmg = (
+			((weaponDamage + attackWeapon.getScopeDamageBonus()) / attackWeapon.getSpeed()) 
+			+ (this.getRangeAttackPower() / 14) 
+			+ attackWeapon.getAmmoDpsBonus()
+			) * attackWeapon.getSpeed();
 	}
+	return dmg;
 }
 
+Combatant.prototype.getMaxDamage = function() {
+	return this.getDamage(this.getAttackWeapon().getMaxDamage());
+}
+Combatant.prototype.getMinDamage = function() {
+	return this.getDamage(this.getAttackWeapon().getMinDamage());
+}
 //
 /// Weapon
 //
-function Weapon() {
-	this._speed = 2.0;
-	this._minDamage = 10.0;
-	this._maxDamage = 15.0;
-	this._isMeleeWeapon = true;//false = ranged
-
+function Weapon(weaponConfig) {
+	this._config = weaponConfig;
 }
-
 
 Weapon.prototype.isMelee = function() {
-	return this._isMeleeWeapon;
+	return this._config.isMeleeWeapon;
 }
-
 Weapon.prototype.getSpeed = function() {
-	//TODO get effective speed of weapon including weapon-mods
-	return this._speed;
+	//TODO Weapon.getSpeed get effective speed of weapon including weapon-mods
+	return this._config.speed;
 }
 Weapon.prototype.getMinDamage = function() {
-	//TODO get effective minDamage of weaponincluding weapon-mods
-	return this._minDamage;
+	//TODO get Weapon.effective minDamage of weaponincluding weapon-mods
+	return this._config.minDamage;
 }
 Weapon.prototype.getMaxDamage = function() {
-	//TODO get effective maxDamage of weapon including weapon-mods
-	return this._maxDamage;
+	//TODO get Weapon.effective maxDamage of weapon including weapon-mods
+	return this._config.maxDamage;
 }
-
-
-
-//
-// TEST
-//
-var attackerMob = new Combatant();
-var defenderMob = new Combatant();
-var ruleBook = new Combatrules();
-
-var result = ruleBook.getAutoAttackResult(attackerMob, defenderMob);
-alert(ruleBook.attackResultToString(result));
+Weapon.prototype.getScopeDamageBonus = function() {
+	//TODO get Weapon.effective Scope Damage Bonus of weapon including all mods
+	return 5;
+}
+Weapon.prototype.getAmmoDpsBonus = function() {
+	//TODO get Weapon.effective Ammo Damage Bonus of weapon including all mods in dps
+	return 1;
+}
+var defaultMeleeWeaponConfig = {
+	isMeleeWeapon : true,
+	maxDamage : 15,
+	minDamage : 10,
+	speed : 2.0
+}
+var heavyMeleeWeaponConfig = {
+	isMeleeWeapon : true,
+	maxDamage : 30,
+	minDamage : 15,
+	speed : 3.0
+}
+var defaultRangeWeaponConfig = {
+	isMeleeWeapon : false,
+	maxDamage : 20,
+	minDamage : 15,
+	speed : 2.5
+}
 
---
+++ js/utils/priority_queue.js
@@ -0,0 +1,182 @@
+/**
+ * PriorityQueue
+ * 
+ * @see https://github.com/STRd6/PriorityQueue.js
+ */
+(function() {
+  /**
+   * @private
+   */
+  var prioritySortHigh = function(a, b) {
+    return b.priority - a.priority; //original
+    //return a <= b ? 1 : -1
+  };
+
+  /**
+   * @private
+   */
+  var prioritySortLow = function(a, b) {
+    return a.priority - b.priority;
+  };
+
+  /*global PriorityQueue */
+  /**
+   * @constructor
+   * @class PriorityQueue manages a queue of elements with priorities. Default
+   * is highest priority first.
+   *
+   * @param [options] If low is set to true returns lowest first.
+   */
+  PriorityQueue = function(options) {
+    var contents = [];
+
+    var sorted = false;
+    var sortStyle;
+
+    if(options && options.low) {
+    	console.log('PriorityQueue created using low-sort');
+      sortStyle = prioritySortLow;
+    } else {
+      sortStyle = prioritySortHigh;
+    }
+
+    /**
+     * @private
+     */
+    var sort = function() {
+      contents.sort(sortStyle);
+      sorted = true;
+    };
+
+    var self = {
+      /**
+       * Removes and returns the next element in the queue.
+       * @member PriorityQueue
+       * @return The next element in the queue. If the queue is empty returns
+       * undefined.
+       *
+       * @see PrioirtyQueue#top
+       */
+      pop: function() {
+        if(!sorted) {
+          sort();
+        }
+
+        var element = contents[0];
+		contents = contents.slice(1, contents.length)
+        if(element) {
+          return element.object;
+        } else {
+          return undefined;
+        }
+      },
+
+      /**
+       * Returns but does not remove the next element in the queue.
+       * @member PriorityQueue
+       * @return The next element in the queue. If the queue is empty returns
+       * undefined.
+       *
+       * @see PriorityQueue#pop
+       */
+      top: function() {
+        if(!sorted) {
+          sort();
+        }
+
+        //var element = contents[contents.length - 1];
+		var element = contents[0];
+        if(element) {
+          return element.object;
+        } else {
+          return undefined;
+        }
+      },
+
+      /**
+       * @member PriorityQueue
+       * @param object The object to check the queue for.
+       * @returns true if the object is in the queue, false otherwise.
+       */
+      includes: function(object) {
+        for(var i = contents.length - 1; i >= 0; i--) {
+          if(contents[i].object === object) {
+            return true;
+          }
+        }
+
+        return false;
+      },
+
+      /**
+       * @member PriorityQueue
+       * @returns the current number of elements in the queue.
+       */
+      size: function() {
+        return contents.length;
+      },
+
+      /**
+       * @member PriorityQueue
+       * @returns true if the queue is empty, false otherwise.
+       */
+      empty: function() {
+        return contents.length === 0;
+      },
+
+      /**
+       * @member PriorityQueue
+       * @param object The object to be pushed onto the queue.
+       * @param priority The priority of the object.
+       */
+      push: function(object, priority) {
+        contents.push({object: object, priority: priority});
+        sorted = false;
+      },
+      
+      //custom methods
+      /**
+       * Adds a priority to all objects (priority cannot become less than 0)
+       * 
+       * @member PriorityQueue
+ 	   * @param priority The priority-delta
+       */
+      addPriorityToAll: function(diff) {
+      	if(!sorted) {
+          sort();
+        }
+        for(var i = contents.length - 1; i >= 0; i--) {
+          contents[i].priority = Math.max(0, (contents[i].priority + diff ));
+        }
+      },
+      
+      getAll: function() {
+      	console.log('getAll invoked');
+      	if(!sorted) {
+        	console.log('getAll sorts');
+      	  	sort();
+        }
+        return contents;
+      },
+      
+      topPriority: function() {
+        if(!sorted) {
+          sort();
+        }
+
+        var element = contents[0];
+
+        if(element) {
+          return element.priority;
+        } else {
+          return undefined;
+        }
+      },
+
+      
+      
+    };
+
+    return self;
+  };
+})();
