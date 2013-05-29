06c3b28773308f41b927274d445fb3dae8c075df
David Latotzky <david-git@latotzky.de>
2013-05-29 23:59:41 +0200

    forgooten fiel in last commit

--- src/scenes/battlefieldScene.js
+++ src/scenes/battlefieldScene.js
@@ -8,7 +8,8 @@ Crafty.scene('battlefield',  function() {
 	var elements = [
         "src/entities/Character.js",
         "src/interfaces/BattleQueue.js",
-        "src/interfaces/MessageDialog.js"
+       "src/interfaces/MessageDialog.js",
+       "src/interfaces/ButtonDialog.js"
 	];
 	
 	//when everything is loaded, run the main scene
@@ -16,40 +17,21 @@ Crafty.scene('battlefield',  function() {
 		Crafty.background("#010");
 		infc['queue'] = new BattleQueue();
 		
-		var pushButtonEntity = Crafty.e("2D, DOM, Text, Mouse, Color")
-            .attr({x: 0, y: 10, z: 1000, w: 100, h:100})
-            .text('push')
-            .textColor('#ffffff')
-            .textFont({'size' : '24px', 'family': 'Arial'})
-            .bind('Click', function(){
-            	addCharacter(generateRandomChar());
-            })
-            .color("red");
 		
-		var popButtonEntity = Crafty.e("2D, DOM, Text, Mouse, Color");//, MouseHover
-	    popButtonEntity
-            .attr({x: 0, y: 120 , z: 1000, w: 100, h:100})
-            .text('pop')
-            .textColor('#ffffff')
-            .textFont({'size' : '24px', 'family': 'Arial'})
-            .bind('Click', function(){
-            	infc['queue'].pop();
-            })
-            .color("yellow");
+		var btnPush = new ButtonDialog({x: 0, y: 10 , text:"push", z:1000});
+		btnPush.getEntity().bind("Click", function(){
+			addCharacter(generateRandomChar());
+            });				
 
-		var msgButtonEntity = Crafty.e("2D, DOM, Text, Mouse, Color");//, MouseHover
-	    msgButtonEntity
-            .attr({x: 0, y: 240 , z: 1000, w: 100, h:100})
-            .text('msg')
-            .textColor('#ffffff')
-            .textFont({'size' : '24px', 'family': 'Arial'})
-            .bind('Click', function(){
-            	message("this is a message");
-            })
-            .color("blue");
-
-	//TODO temporary display button to execute next attack				
-
+		var btnPop = new ButtonDialog({x: 0, y: 120 , text:"pop", z:1000});
+		btnPop.getEntity().bind("Click", function(){
+			infc['queue'].pop();
+            });				
+		var btnTestMsg = new ButtonDialog({x: 0, y: 240, text:"msg", z:1000});
+		btnTestMsg.getEntity().bind("Click", function(){
+			message("this is also message");
+		});
+		
 	
 	});
 	
@@ -59,7 +41,7 @@ function message(txt) {
 	infc['msg'] = new MessageDialog({text: txt, w:400, h:200, x:150, y:70});	
 	infc['msg'].getEntity().bind('Click', function(data){
         	infc['msg'].remove();
-        	infc['msg'] =null;
+        	infc['msg'] =null;	
         });
 //	infc['msg'].getEntity().attr({w:100, h:400});
 }
@@ -80,7 +62,6 @@ function generateRandomChar() {
     var c = new Character();
     c.set({'characterId': newCharId});
     c.set({'name': names[Crafty.math.randomInt(0,names.length-1)] + ' ' +lastnames[Crafty.math.randomInt(0,lastnames.length-1)]});
-    
     
     return c;
 }
